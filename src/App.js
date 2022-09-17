94373f128e64b1f8875af9078786cdefa2c828aa
ClaudeNi <claudenij2@gmail.com>
2022-09-17 20:02:43 +0300

    added letters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Letter from "./components/Letter/Letter";
 import "./App.css";
 
 function App() {
+	let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	const displayLetters = () => {
+		return letters.split("").map((char, i) => {
+			return <Letter letter={char} />;
+		});
+	};
 	return (
 		<div className="App">
 			<h1>hangman</h1>
+			<div>{displayLetters()}</div>
 		</div>
 	);
 }
---
+++ src/components/Letter/Letter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Letter = (props) => {
+	return <div>{props.letter}</div>;
+};
+
+export default Letter;
