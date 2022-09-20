3af48fd75ab03c648ca212629918989321497b40
ClaudeNi <claudenij2@gmail.com>
2022-09-20 17:22:04 +0300

    started work on letter click handle

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import { useState } from "react";
 
 function App() {
 	const [word, setWord] = useState("");
+	const [guesses, setGuesses] = useState([]);
+
 	let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	const letterClickHandle = (guess) => {
+		if (!guesses.includes(guess)) {
+			let newGuesses = guesses;
+			console.log(newGuesses);
+			newGuesses.push(guess);
+			setGuesses(newGuesses);
+			setWord(word);
+		}
+	};
 
 	const displayLetters = () => {
 		return letters.split("").map((char, i) => {
-			return <Letter letter={char} key={i} />;
+			return (
+				<Letter letter={char} key={i} clickHandle={letterClickHandle} />
+			);
 		});
 	};
 
 	const displayLines = () => {
 		return word.split("").map((char, i) => {
+			if (guesses.includes(char.toUpperCase())) {
+				return <span key={i}>{char}</span>;
+			}
 			return (
 				<span char={char} key={i}>
 					{" "}
@@ -27,7 +44,7 @@ function App() {
 			<h1 className="title">Hangman</h1>
 			{displayLines()}
 			<div className="letters-container">{displayLetters()}</div>
-			<button onClick={() => setWord("word")}>set word</button>
+			<button onClick={() => setWord("Word")}>set word</button>
 		</div>
 	);
 }
--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./letter.css";
 
 const Letter = (props) => {
-	return <div className="letter">{props.letter}</div>;
+	return (
+		<div
+			role={"button"}
+			className="letter"
+			onClick={() => {
+				props.clickHandle(props.letter);
+			}}
+		>
+			{props.letter}
+		</div>
+	);
 };
 
 export default Letter;
